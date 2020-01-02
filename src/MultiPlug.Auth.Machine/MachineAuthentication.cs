73fe6041fcc1bbbe9947ac5f565e3d5dded6ef04
British Systems <41692418+British-Systems@users.noreply.github.com>
2020-01-02 17:41:50 +0000

    1.0.0

---
+++ src/MultiPlug.Auth.Machine/MachineAuthentication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using MultiPlug.Base.Security;
+
+namespace MultiPlug.Auth.Machine
+{
+    public class MachineAuthentication : IAuthentication
+    {
+        private string[] m_Domains;
+
+        public MachineAuthentication()
+        {
+            m_Domains = Environment.OSVersion.Platform == PlatformID.Unix ? new string[0] : new string[] { "Machine" };
+        }
+
+        public IReadOnlyCollection<string> Domains
+        {
+            get
+            {
+                return Array.AsReadOnly(m_Domains);
+            }
+        }
+
+        public AuthResult Authenticate(AuthCredentials theCredentials)
+        {
+            bool Result = false;
+            string Message = "OK";
+
+            try
+            {
+                using (PrincipalContext Context = new PrincipalContext(ContextType.Machine))
+                {
+                    try
+                    {
+                        Result = Context.ValidateCredentials(theCredentials.Username, theCredentials.Password);
+                    }
+                    catch (System.Runtime.InteropServices.COMException)
+                    {
+                    }
+                    catch (ArgumentException)
+                    {
+                    }
+                    catch (Exception ex)
+                    {
+                        Message = ex.Message;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+
+            return new AuthResult(Result, Message);
+        }
+    }
+}
