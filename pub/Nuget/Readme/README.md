77c857f5809c59aa76071b5c936ccbb875740289
british-systems <github@britishsystems.uk>
2024-10-29 22:43:01 +0000

    #1 Release 1.0.3

---
+++ pub/Nuget/Readme/README.md
@@ -0,0 +1,7 @@
+# Functionality
+
+Used for the MultiPlug Edge Computing Platform - https://www.multiplug.app/
+
+# For
+
+* Microsoft Windows machine based authentication for the MultiPlug .Net Edge Computing Platform
--- src/MultiPlug.Auth.Machine/MachineAuthentication.cs
+++ src/MultiPlug.Auth.Machine/MachineAuthentication.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.DirectoryServices.AccountManagement;
 using MultiPlug.Base.Security;
+using System.Management;
+using System.DirectoryServices;
 
 namespace MultiPlug.Auth.Machine
 {
@@ -17,6 +19,87 @@ namespace MultiPlug.Auth.Machine
         public MachineAuthentication()
         {
             m_Domains = Environment.OSVersion.Platform == PlatformID.Unix ? new string[0] : new string[] { "Machine" };
+        }
+
+        public IAuthResult Add(IAuthCredentials theCredentials)
+        {
+            if(Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                return new AuthResult(false, string.Empty, "Operation only supported on Windows");
+            }
+
+            try
+            {
+                DirectoryEntry AD = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
+                DirectoryEntry NewUser = AD.Children.Add(theCredentials.Username, "user");
+                NewUser.Invoke("SetPassword", new object[] { theCredentials.Password });
+                NewUser.Invoke("Put", new object[] { "Description", "User created by MultiPlug" });
+                NewUser.CommitChanges();
+                DirectoryEntry grp;
+
+                grp = AD.Children.Find("Guests", "group");
+                if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+
+                return new AuthResult(true, theCredentials.Username, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                return new AuthResult(false, string.Empty, ex.Message);
+            }
+        }
+
+        public IAuthResult Edit(IAuthCredentials theCredentials, IAuthCredentials theNewCredentials)
+        {
+            return new AuthResult(false, string.Empty, "Use Windows to change users or passwords");
+        }
+
+        public IAuthResult Delete(IAuthCredentials theCredentials)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                return new AuthResult(false, string.Empty, "Operation only supported on Windows");
+            }
+
+            try
+            {
+                DirectoryEntry AD = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
+                DirectoryEntries entries = AD.Children;
+                DirectoryEntry user = entries.Find(theCredentials.Username);
+                entries.Remove(user);
+
+                return new AuthResult(true, theCredentials.Username, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                return new AuthResult(false, string.Empty, ex.Message);
+            }
+
+        }
+
+        public IReadOnlyCollection<string> Users()
+        {
+            List<string> UserList;
+
+            if ( Environment.OSVersion.Platform != PlatformID.Unix )
+            {
+                SelectQuery query = new SelectQuery("Win32_UserAccount");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+                var SearchResult = searcher.Get();
+
+                UserList = new List<string>(SearchResult.Count);
+
+                foreach (ManagementObject envVar in SearchResult)
+                {
+                    UserList.Add(m_Domains[0] + "\\" + envVar["Name"].ToString());
+                }
+            }
+            else
+            {
+                UserList = new List<string>(0);
+            }
+
+            return Array.AsReadOnly(UserList.ToArray());
         }
 
         public IReadOnlyCollection<string> Domains
